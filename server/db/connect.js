0a3ed0a1d1ef26f448bbd94365ba7e42fc1b1c40
Ryan Gouldsmith <RyanGouldsmith@gmail.com>
2018-12-19 20:31:15 +0000

    Bump dependency and add a log to the error handler on mongo

--- server/db/connect.js
+++ server/db/connect.js
@@ -7,7 +7,7 @@ const { HOSTNAME, PORT, DB } = process.env;
 export function connectDatabase() {
   Mongoose.connect(`mongodb://${HOSTNAME}:${PORT}/${DB}`);
   Mongoose.connection.on('connected', () => console.log('connected'));
-  Mongoose.connection.on('error', () => console.log('error'));
+  Mongoose.connection.on('error', error => console.log('error', error));
 }
 
 export const Spending = Mongoose.model('Spending', SpendingSchema, 'spending');
